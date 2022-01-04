349505361e47d719dea2173f128ffdbad82fe1f0
AntonShvets0 <45802050+AntonShvets0@users.noreply.github.com>
2022-01-04 12:12:30 +0200

    Update play.py
    fix

--- play.py
+++ play.py
@@ -1,7 +1,7 @@
 from gpt2generator import get_generator
 import random, os
 
-f = open("data/seed.txt", "r")
+f = open("./data/seed.txt", "r")
 texts = f.read().split("\n")
 f.close()
 
@@ -11,7 +11,7 @@ context = input()
 print("Enter count text:")
 count = int(input())
 
-models = os.walk("models/")
+models = os.walk("./models/")
 generator = get_generator(models[0], generate_num=60, temperature=0.6, top_k=40, top_p=0.9, repetition_penalty=1)
 
 f = open("result.txt", "rw+")
